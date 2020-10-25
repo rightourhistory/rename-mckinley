2d3d9af00070c521c8d111865f805f46086e056e
Taylor Ho <taylorkmho@gmail.com>
2020-10-24 18:46:01 -1000

    added siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Rename McKinley High School`,
+    siteUrl: `http://127.0.0.1`,
+    description: `A movement for the people of Hawai'i`,
+  },
   plugins: [],
 }
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return <div>
+    <Header />
+    Hell!o about!
+  </div>
+}
