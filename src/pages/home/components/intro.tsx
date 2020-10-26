d653c8566f3f8a65fe328c5e3fd9af77ee5fa1f6
Taylor Ho <taylorkmho@gmail.com>
2020-10-25 22:16:45 -1000

    added IntroAssociation

--- src/pages/home/components/intro.tsx
+++ src/pages/home/components/intro.tsx
@@ -6,6 +6,7 @@ export default class Intro extends React.Component {
     return ( 
       <div className="intro">
         <IntroPolicy />
+        <IntroAssociation />
       </div>
     )
   }
@@ -23,6 +24,30 @@ class IntroPolicy extends React.Component {
             The name of a school facility shall be a unique identifier... to identify the facility in other ways that bear ✨ positive association ✨ for the school, school-community, or public education.
           </blockquote>
           <p className="intro-policy__subtext">[EMOJI ADDED FOR EMPHASIS]</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+class IntroAssociation extends React.Component {
+  render() {
+    return (
+      <div className="intro-association">
+        <div className="intro-association__container">
+          <div className="intro-association__column intro-association__column--1">
+            <img src="https://via.placeholder.com/280x260.png/AC1D11/AC1D11" />
+            <p>Well, let’s just say <s>William McKinley</s>’s association with the community of Hawai‘i has a less than positive history.</p>
+            <p>In 1897, this man thrusted Hawai‘i into the United States when he illegally* annexed the kingdom for its natural resources and strategic military location.</p>
+            <img src="https://via.placeholder.com/400x400.png/AC1D11/AC1D11" />
+          </div>
+          <div className="intro-association__column intro-association__column--2">
+            <img src="https://via.placeholder.com/480x320.png/AC1D11/AC1D11" />
+            <img src="https://via.placeholder.com/280x260.png/AC1D11/AC1D11" />
+            <p>The native Alaskan community and its allies fought for a similar naming resolution with Mount <s>McKinley</s> for decades.</p>
+            <p>After over 100 years, they succeeded*.</p>
+            <p>It is now Hawai‘i’s turn! Eo!</p>
+          </div>
         </div>
       </div>
     )
