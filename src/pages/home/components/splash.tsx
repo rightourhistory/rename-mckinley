4ec2a5a7e047333219bf50b85c5f1fb554530507
taylorkmho <taylorkmho@gmail.com>
2020-11-15 21:37:26 -0800

    fancier splash

--- src/pages/home/components/splash.tsx
+++ src/pages/home/components/splash.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./splash.scss";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 export default class Splash extends React.Component {
   render() {
@@ -7,14 +10,38 @@ export default class Splash extends React.Component {
       <div className="splash">
         <div className="splash__container">
           <h1 className="splash__headline">
-            Rename <em>McKinley</em> High School
+            <span className="splash-headline splash-headline--1">Rename</span>{" "}
+            <span className="splash-headline splash-headline--2">
+              <em>McKinley</em>
+            </span>{" "}
+            <span className="splash-headline splash-headline--3">
+              High School
+            </span>
           </h1>
-          <a className="splash__cta" href="#">
+          {/* <a className="splash__cta" href="#">
             Sign the Petition
-          </a>
+          </a> */}
         </div>
         {this.props.children}
       </div>
     );
+  }
+  componentDidMount() {
+    let headlineText = document.querySelector(".splash-headline--2");
+    if (!headlineText) return;
+    let scrollTrigger = {
+      trigger: ".splash-headline--2",
+      start: "center center",
+      end: "center -400px",
+      scrub: true
+    };
+    gsap.to(headlineText, {
+      scrollTrigger: scrollTrigger,
+      x: -300,
+      color: "#ced3cd",
+      opacity: 0,
+      ease: "none",
+      duration: 1
+    });
   }
 }
