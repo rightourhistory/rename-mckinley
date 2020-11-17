cc90bfc9fd12b782ec10b36774e8f8750a200172
taylorkmho <taylorkmho@gmail.com>
2020-11-16 20:54:51 -0800

    updating splash fontSize on window resize

--- src/pages/home/components/splash.tsx
+++ src/pages/home/components/splash.tsx
@@ -2,19 +2,44 @@ import React from "react";
 import "./splash.scss";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import { throttle } from "../../../helpers";
 gsap.registerPlugin(ScrollTrigger);
 
 export default class Splash extends React.Component {
+  private textRefsArray: (HTMLSpanElement | null)[];
+
+  constructor(props: any) {
+    super(props);
+    this.textRefsArray = [];
+    this.setFontSize = this.setFontSize.bind(this);
+  }
   render() {
     return (
       <div className="splash">
         <div className="splash__container">
           <h1 className="splash__headline">
-            <span className="splash-headline splash-headline--1">Rename</span>{" "}
-            <span className="splash-headline splash-headline--2">
+            <span
+              className="splash-headline splash-headline--1"
+              ref={ref => {
+                this.textRefsArray[0] = ref;
+              }}
+            >
+              Rename
+            </span>{" "}
+            <span
+              className="splash-headline splash-headline--2"
+              ref={ref => {
+                this.textRefsArray[1] = ref;
+              }}
+            >
               <em>McKinley</em>
             </span>{" "}
-            <span className="splash-headline splash-headline--3">
+            <span
+              className="splash-headline splash-headline--3"
+              ref={ref => {
+                this.textRefsArray[2] = ref;
+              }}
+            >
               High School
             </span>
           </h1>
@@ -27,6 +52,12 @@ export default class Splash extends React.Component {
     );
   }
   componentDidMount() {
+    this.setHeaderGsap();
+    this.setFontSize();
+    const throttledSetFontSize = throttle(this.setFontSize, 75);
+    window.onresize = throttledSetFontSize;
+  }
+  setHeaderGsap() {
     let headlineText = document.querySelector(".splash-headline--2");
     if (!headlineText) return;
     let scrollTrigger = {
@@ -40,6 +71,14 @@ export default class Splash extends React.Component {
       opacity: 0,
       ease: "none",
       duration: 1
+    });
+  }
+  setFontSize() {
+    this.textRefsArray.forEach(textRef => {
+      textRef!.style.fontSize = null;
+      textRef!.style.fontSize = window.getComputedStyle(
+        this.textRefsArray[0]!
+      ).fontSize;
     });
   }
 }
