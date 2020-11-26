b1397710ecccab8094a90e8e22380a4e2dd90595
taylorkmho <taylorkmho@gmail.com>
2020-11-25 23:00:35 -0800

    update site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   siteMetadata: {
     title: "Rename McKinley High School",
-    siteUrl: "http://127.0.0.1",
+    siteUrl: "http://rightourhistoryhawaii.com",
     description: "A movement for the people of Hawai'i"
   },
   plugins: [
